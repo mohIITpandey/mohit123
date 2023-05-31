11cff4f61cf829080a548e52bb7b6ac169d6d5bb
admin <admin@MOHIT>
2023-05-31 16:58:57 +0530

    jyfyjg

--- src/test/java/DemoWai.java
+++ src/test/java/DemoWai.java
@@ -1,12 +1,36 @@
+import java.time.Duration;
+import java.util.List;
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoWai {
 public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Search phone:");
+	String searchPhone = sc.next();
+    String searchConnectivity = sc.next();
+    
 	WebDriver driver= new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	driver.get("https://www.amazon.com");
 	driver.findElement(By.id("twotabsearchtextbox")).sendKeys("samsung phones");
+	List<WebElement> lists = driver.findElements(By.xpath("//div[@class='s-suggestion s-suggestion-ellipsis-direction'] "));
+	for(int i=0;i<lists.size();i++) {
+		String text = lists.get(i).getText();
+		if(text.equalsIgnoreCase(searchPhone)) {
+			lists.get(i).click();
+		}
+	}
+	if(searchConnectivity.contains("USB"))
+		searchConnectivity(driver, "USB").click();
+}
+public static WebElement searchConnectivity(WebDriver driver,String connectivity) {
+	WebElement Connectivity = driver.findElement(By.xpath("//span[contains(.,'Mobile Phone Connectivity Technology')]/parent::div[contains(@id,'feature')]/following-sibling::ul//li[@aria-label='"+connectivity+"']//input[@type='checkbox']"));
+	return Connectivity;
 }
  
 }
